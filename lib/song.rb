9831b146b7e667151caa761104c209bffb931949
Mark Hernandez <m.hern1415@gmail.com>
2019-05-13 22:55:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,7 +4,7 @@ class Song
   
 attr_accessor :name, :artist, :genre 
 
-def initialize(song, artist, genre)
+def initialize(name, artist, genre)
   @song = song 
   @artist = artist 
   @genre = genre 
