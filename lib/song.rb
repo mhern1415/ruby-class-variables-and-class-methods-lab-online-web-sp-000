7c47308d3810663d9499d232880bd05fd321e487
Mark Hernandez <m.hern1415@gmail.com>
2019-05-13 22:58:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,18 +1,18 @@
 class Song 
 
-  @@song_count = 0 
+  @@count = 0 
   
 attr_accessor :name, :artist, :genre 
 
-def initialize(name, artist, genre)
-  @name = name 
-  @artist = artist 
-  @genre = genre 
-  @@song_count += 1 
-end
+  def initialize(name, artist, genre)
+   @name = name 
+    @artist = artist 
+    @genre = genre 
+    @@count += 1 
+  end
   
   def count 
-    @@song_count
+    @@count
   end
   
 end
